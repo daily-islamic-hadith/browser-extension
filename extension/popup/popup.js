4359752c323a6aee474a493e396540ab3430b543
Mohamed Osama <mohamed.osama@booking.com>
2024-09-17 01:36:56 +0200

    adjust id in manifest

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const hadithElement = document.getElementById('hadith');
 
     function fetchHadith() {
-        fetch('https://marine-diane-daily-hadith-c60e1c0d.koyeb.app/api/today-hadith')
+        fetch('https://dailyislamichadith.com/api/today-hadith')
             .then(response => response.json())
             .then(data => {
                 chrome.storage.local.get(
